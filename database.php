b41b03f577309d6627ee2e299e1cf6ab1a4781a3
ekoester96 <ethan.schmidt1996@gmail.com>
2025-09-15 11:57:19 -0500

    Create database.php

---
+++ database.php
@@ -0,0 +1,18 @@
+<?php
+$host = 'localhost';
+$dbname = 'insecure_app_db';
+$username = 'php_user';
+$password = 'P@ssword123!'; // The password you set in the SQL commands
+
+try {
+    // Establish the connection to MySQL
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+} catch(PDOException $e) {
+    // If connection fails, stop the script and show an error
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+?>
